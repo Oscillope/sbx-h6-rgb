2159e7c863b376185f4fa51bf648997c92da6ec7
Jason Rosenman <jvrosenman@gmail.com>
2020-06-14 13:55:13 -0400

    Add brightness option

--- sbx-h6-ctl.c
+++ sbx-h6-ctl.c
@@ -10,6 +10,7 @@ static char doc[] = "SoundblasterX H6 LED/DSP Control\n\tCurrently only allows s
 static char args_doc[] = "vendorId:productId";
 static struct argp_option options[] = {
 	{"color",	'c',	"HEX_COLOR",	0,	"Hex color to display on the headset's LED rings.", 0},
+	{"brightness",	'b',	"VALUE",	0,	"LED brightness (0-255)", 0},
 	{ 0 }
 };
 
@@ -18,6 +19,7 @@ struct sbx_h6_settings {
 	uint32_t product_id;
 	libusb_device_handle *handle;
 	uint32_t color;
+	uint8_t brightness;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state)
@@ -25,6 +27,9 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 	struct sbx_h6_settings *settings = state->input;
 	int err = 0;
 	switch (key) {
+	case 'b':
+		settings->brightness = strtol(arg, NULL, 0);
+		break;
 	case 'c':
 		settings->color = strtol(arg, NULL, 16);
 		break;
@@ -47,11 +52,27 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 static struct argp argp_setup = { options, parse_opt, args_doc, doc, NULL, NULL, NULL };
 
 // Helper function to convert from RGB to RBG (headset takes RBG)
-uint32_t sbx_rgb_to_rbg(uint32_t color)
+uint32_t sbx_rgb_to_rbg(uint32_t color, uint8_t brightness)
 {
-	uint8_t r = (color & 0xff0000) >> 16;
-	uint8_t g = (color & 0x00ff00) >> 8;
+	uint8_t bright = (0xff - brightness);
+	uint8_t r = ((color & 0xff0000) >> 16);
+	uint8_t g = ((color & 0x00ff00) >> 8);
 	uint8_t b = (color & 0x0000ff);
+	if (bright > r) {
+		r = 0;
+	} else {
+		r -= bright;
+	}
+	if (bright > b) {
+		b = 0;
+	} else {
+		b -= bright;
+	}
+	if (bright > g) {
+		g = 0;
+	} else {
+		g -= bright;
+	}
 	return (r | (b << 8) | (g << 16));
 }
 
@@ -75,7 +96,7 @@ int sbx_set_color(struct sbx_h6_settings *settings)
 	}
 	*(uint32_t*)(buf + SBX_H6_CMD_INDEX) = htonl(SBX_H6_COLOR_CMD);
 	// The actual color goes in these 3 bytes
-	*(uint32_t*)(buf + SBX_H6_COLOR_INDEX) = sbx_rgb_to_rbg(settings->color);
+	*(uint32_t*)(buf + SBX_H6_COLOR_INDEX) = sbx_rgb_to_rbg(settings->color, settings->brightness);
 	err = libusb_control_transfer(settings->handle, 0x21, 9, 0x02ff, 3, buf, SBX_H6_CMD_LEN, 0);
 	if (err < 0) {
 		printf("err %d during color transfer\n", err);
@@ -96,6 +117,7 @@ int main(int argc, char **argv)
 	struct sbx_h6_settings headset;
 	int err = 0;
 	memset(&headset, 0, sizeof(headset));
+	headset.brightness = 0xff;
 	argp_parse(&argp_setup, argc, argv, 0, 0, &headset);
 
 	err = libusb_init(NULL);
